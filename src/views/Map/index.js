c719e5140dadeb05a81f17d1ba5bec83e55d865b
Pawel Kozera <ztraktor2@gmail.com>
2023-12-05 20:51:39 +0100

    Added map display to Map view

--- src/views/Map/index.js
+++ src/views/Map/index.js
@@ -1,10 +1,40 @@
-import {Text, View} from "react-native";
-
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
 
 export function Map({ navigation }) {
+  const Map = () => {
+    const [region, setRegion] = useState({
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+
+    const [coordinates, setCoordinates] = useState([]);
+
+    useEffect(() => {
+      const mockLocation = { latitude: 37.7749, longitude: -122.4194 };
+      setRegion({ ...region, ...mockLocation });
+    }, []);
+
+    const onRegionChange = (newRegion) => {
+      setCoordinates((prevCoordinates) => [
+        ...prevCoordinates,
+        { latitude: newRegion.latitude, longitude: newRegion.longitude },
+      ]);
+    };
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Map</Text>
-        </View>
+      <MapView
+        style={{ flex: 1 }}
+        region={region}
+        onRegionChangeComplete={onRegionChange}
+      >
+        <Marker coordinate={region} title="Your Location" />
+        <Polyline coordinates={coordinates} strokeWidth={5} strokeColor="blue" />
+      </MapView>
     );
+  };
+
+  return <Map />;
 }
