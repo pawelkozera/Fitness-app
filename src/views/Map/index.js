77d0dc9e95dca39d7fa0cf875681eda3af9809c5
Pawel Kozera <ztraktor2@gmail.com>
2023-12-05 21:26:38 +0100

    Displaying user location to the screen, test tracking system

--- src/views/Map/index.js
+++ src/views/Map/index.js
@@ -1,40 +1,93 @@
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import React, { useEffect, useState } from 'react';
+import { Accelerometer, Magnetometer } from 'expo-sensors';
+import * as Location from 'expo-location';
 
 export function Map({ navigation }) {
-  const Map = () => {
-    const [region, setRegion] = useState({
-      latitude: 0,
-      longitude: 0,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+  const [region, setRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [coordinates, setCoordinates] = useState([]);
+  const [isMoving, setIsMoving] = useState(false);
+  const [heading, setHeading] = useState(0);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.error('Permission to access location was denied');
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        setRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        });
+      } catch (error) {
+        console.error('Error getting location:', error);
+      }
+    };
+
+    getLocation();
+
+    const accelerometerSubscription = Accelerometer.addListener(({ x, y, z }) => {
+      const movementThreshold = 1.5;
+      const isDeviceMoving = Math.abs(x) > movementThreshold || Math.abs(y) > movementThreshold || Math.abs(z) > movementThreshold;
+
+      setIsMoving(isDeviceMoving);
     });
 
-    const [coordinates, setCoordinates] = useState([]);
+    const magnetometerSubscription = Magnetometer.addListener(({ x, y, z }) => {
+      const newHeading = Math.round((Math.atan2(y, x) * 180) / Math.PI);
+      setHeading(newHeading >= 0 ? newHeading : newHeading + 360);
+    });
 
-    useEffect(() => {
-      const mockLocation = { latitude: 37.7749, longitude: -122.4194 };
-      setRegion({ ...region, ...mockLocation });
-    }, []);
+    return () => {
+      accelerometerSubscription.remove();
+      magnetometerSubscription.remove();
+    };
+  }, []);
 
-    const onRegionChange = (newRegion) => {
+  const onRegionChange = (newRegion) => {
+    if (isMoving) {
       setCoordinates((prevCoordinates) => [
         ...prevCoordinates,
         { latitude: newRegion.latitude, longitude: newRegion.longitude },
       ]);
-    };
-
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        region={region}
-        onRegionChangeComplete={onRegionChange}
-      >
-        <Marker coordinate={region} title="Your Location" />
-        <Polyline coordinates={coordinates} strokeWidth={5} strokeColor="blue" />
-      </MapView>
-    );
+    }
   };
 
-  return <Map />;
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      region={region}
+      onRegionChangeComplete={onRegionChange}
+      showsUserLocation={true}
+      followsUserLocation={true}
+      rotateEnabled={false}
+      showsMyLocationButton={false}
+      provider="google"
+      customMapStyle={mapStyle} 
+      heading={heading}
+    >
+      <Polyline coordinates={coordinates} strokeWidth={5} strokeColor="blue" />
+    </MapView>
+  );
 }
+
+const mapStyle = [
+  {
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }],
+  },
+];
