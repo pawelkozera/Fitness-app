44a68ca278368f1f180343d09420f035e900cffe
Pawel Kozera <ztraktor2@gmail.com>
2024-01-09 19:04:24 +0100

    Added timer to the ExerciseDetails view, user can set up timer for 30s, 1 min or 2 min

--- src/views/ExerciseDetails/index.js
+++ src/views/ExerciseDetails/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { serverConfig } from '../../config/config';
 
@@ -30,11 +30,46 @@ export function ExerciseDetails({ route, navigation }) {
   const { exerciseDetails } = route.params;
 
   const selectedImage = imageMap[exerciseDetails.imgName];
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        if (seconds > 0) {
+            setSeconds((prevSeconds) => prevSeconds - 1);
+        }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
+
+  const setTimer = (duration) => {
+    setSeconds(duration);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
 
   return (
     <View style={theme.background}>
       <View style={theme.container}>
         <Image source={selectedImage} style={styles.image} />
+        
+        <View style={styles.timerContainer}>
+          <Text style={styles.timerText}>{formatTime(seconds)}</Text>
+
+          <TouchableOpacity style={styles.button} onPress={() => setTimer(30)}>
+            <Text>30s</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setTimer(60)}>
+            <Text>1m</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setTimer(120)}>
+            <Text>2m</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -46,5 +81,25 @@ const styles = StyleSheet.create({
     height: '90%',
     resizeMode: 'cover',
     marginBottom: 100,
+  },
+  timerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#059669',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+    textAlign: 'center',
+    alignContent: 'center',
+    borderRadius: 50,
+  },
+  timerText: {
+    marginTop: 10,
+    marginRight: 30,
   },
 });
