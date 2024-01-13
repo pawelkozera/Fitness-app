88d2791975cf5a9dbc5bba39235c6ce3e88bbca0
Pawel Kozera <ztraktor2@gmail.com>
2024-01-13 12:38:27 +0100

    Added basic view for WeeklyAchievements

--- src/views/WeeklyAchievements/index.js
+++ src/views/WeeklyAchievements/index.js
@@ -1,15 +1,71 @@
-import {Text, View} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Text, ToastAndroid, View } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
-
+import { serverConfig } from '../../config/config';
 
 export function WeeklyAchievements({ navigation }) {
-    const {theme} = useTheme();
+  const { theme } = useTheme();
+  const [trainings, setTrainings] = useState(null);
 
-    return (
-        <View style={theme.background}>
-            <View style={theme.container}>
-                <Text style={theme.text}>WeeklyAchievements</Text>
-            </View>
-        </View>
-    ); 
+  useEffect(() => {
+    const fetchTrainings = async () => {
+      try {
+        const response = await fetch(`${serverConfig.apiUrl}:${serverConfig.port}/trainings`);
+        const data = await response.json();
+        setTrainings(data);
+      } catch (error) {
+        console.error('Błąd podczas pobierania danych treningowych', error);
+      }
+    };
+
+    fetchTrainings();
+  }, []);
+
+  const calculateWeeklySummary = () => {
+    if (trainings === null) {
+      return {
+        distance: 0,
+        calories: 0,
+        duration: 0,
+        pace: 0,
+      };
+    }
+
+    const today = new Date();
+    const lastMonday = new Date(today);
+    lastMonday.setDate(today.getDate() - ((today.getDay() + 6) % 7));
+
+    const weeklyTrainings = trainings.filter(training => {
+      const trainingDate = new Date(training.date);
+      return trainingDate >= lastMonday && trainingDate <= today;
+    });
+
+    const summary = {
+      distance: weeklyTrainings.reduce((sum, training) => sum + training.distance, 0),
+      calories: weeklyTrainings.reduce((sum, training) => sum + training.calories, 0),
+      duration: weeklyTrainings.reduce((sum, training) => sum + training.duration, 0),
+      pace: weeklyTrainings.length > 0 ? weeklyTrainings.reduce((sum, training) => sum + training.pace, 0) / weeklyTrainings.length : 0,
+    };
+
+    return summary;
+  };
+
+  const weeklySummary = calculateWeeklySummary();
+
+  const todayDate = new Date();
+  let lastMonday = new Date(todayDate);
+  lastMonday.setDate(todayDate.getDate() - ((todayDate.getDay() + 6) % 7));
+
+  return (
+    <View style={theme.background}>
+      <View style={theme.container}>
+      <Text style={theme.text}> {lastMonday.toLocaleDateString('pl-PL', {month: 'numeric', day: 'numeric'})} - {todayDate.toLocaleDateString('pl-PL', { month: 'numeric', day: 'numeric' })}</Text>
+
+        <Text style={theme.text}>Distance: {weeklySummary.distance} km</Text>
+        <Text style={theme.text}>Calories: {weeklySummary.calories} kcal</Text>
+        <Text style={theme.text}>Duration: {weeklySummary.duration} minutes</Text>
+        <Text style={theme.text}>Average Pace: {weeklySummary.pace.toFixed(2)} km/h</Text>
+      </View>
+    </View>
+  );
 }
